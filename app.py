a6feba09666cdc2cab49ad5d3a688572cb7282a0
Irinus <irinamayborodina@gmail.com>
2026-01-27 18:37:07 +0300

    base_normal

--- app.py
+++ app.py
@@ -16,13 +16,26 @@ initial_df = pd.DataFrame({
 })
 
 
+def update_visibility():
+    return gr.update(visible=True), gr.update(visible=True)
+
+
+def update_invisibility():
+    return gr.update(visible=False), gr.update(visible=False)
+
+
 with gr.Blocks() as demo:
     with gr.Tab("Симуляция звонка"):
-        gr.Markdown("### <center>Нажмите на кнопку ниже, чтобы начать звонок")
-        call_btn = gr.Button("Позвонить")
-        hidden_recorder = gr.Audio(sources=["microphone"], streaming=True, type="numpy", visible=False, interactive=True, elem_id="recorder")
+        gr.Markdown("### <center>Нажмите на кнопку ниже, чтобы начать звонок", visible=False)
+        call_btn = gr.Button("Позвонить", visible=True)
+        recorder_text = gr.Markdown("Нажмите на кнопку записи, чтобы описать проблему устно.", visible=False)
+        recorder = gr.Audio(sources=["microphone"], streaming=True, type="numpy", visible=False, interactive=True, elem_id="recorder")
+        recorder_final_text = gr.Markdown("Затем завершите запись, когда будете готовы.", visible=False)
         audio_state = gr.State(value={"full_buffer": [], "partial_buffer": []})  # cостояние для накопления чанков
-        hidden_player = gr.Audio(format="mp3", visible=False, autoplay=False, interactive=False, elem_id="player")
+        player_start_text = gr.Markdown("Нажмите на кнопку воспроизведения, чтобы прослушать приветствие.", visible=False)
+        player_end_text = gr.Markdown("Нажмите на кнопку воспроизведения, чтобы прослушать финальное сообщение.", visible=False)
+        player1 = gr.Audio(format="mp3", visible=False, autoplay=False, interactive=True, elem_id="player")
+        player2 = gr.Audio(format="mp3", visible=False, autoplay=False, interactive=True, elem_id="player")
         routing_result = gr.Textbox(label="Подобранный специалист", visible=False)
     with gr.Tab("Панель специалиста"):
         gr.Markdown("### <center>Описание проблемы клиента:")
@@ -34,67 +47,61 @@ with gr.Blocks() as demo:
             label="Список сотрудников")
         admin_button = gr.Button("Сохранить изменения")
 
-    # при нажатии на кнопку звонка
+
+    # 1. ВКЛАДКА С ИМИТАЦИЕЙ ЗВОНКА
+    # при нажатии на кнопку звонка (появляется проигрыватель с приветственным аудио)
     call_btn.click(
         fn=lambda: call.welcome_audio_path,
-        outputs=hidden_player
+        outputs=player1
     ).then(
-        js=f"""
-            () => {{
-                const player = document.querySelector('#player audio');
-                if (!player) return;
-    
-                player.play().catch(e => console.log("play error:", e));
-    
-                setTimeout(() => {{
-                    const btn = document.querySelector('#recorder button');
-                    if (btn) btn.click();
-                }}, {call.delay_ms});
-            }}
-            """
+        fn=lambda: gr.update(visible=False),
+        outputs=call_btn
     ).then(
-        js="""
-        () => {
-            const recorder = document.querySelector('#recorder');
-            if (!recorder) return;
+        fn=update_visibility,
+        outputs=[player_start_text, player1]
+    )
 
-            // сомнительно
-            const stopBtn = recorder.querySelectorAll('button')[1];  // или 'button[aria-label="Stop Recording"]'
-            const stopBtn = buttons.length >= 2 ? buttons[1] : null;
-            if (stopBtn && stopBtn.innerHTML.includes('square') || stopBtn.textContent.trim() === '') {
-                stopBtn.click();
-            }
-
-            setTimeout(() => {
-                if (stopBtn) {
-                    stopBtn.click();
-                    console.log("Авто-остановка записи через 120 секунд");
-                }
-            }, 120000);
-        }
-        """
+    # когда воспроизведение записи останавливается (появляется рекордер)
+    player1.stop(
+        fn=update_invisibility,
+        outputs=[player_start_text, player1]
+    ).then(
+        fn=lambda: [gr.update(visible=True), gr.update(visible=True), gr.update(visible=True)],
+        outputs=[recorder_text, recorder, recorder_final_text]
     )
 
     # стриминг с микрофона
-    hidden_recorder.stream(
+    recorder.stream(
         fn=call.process_partial_chunk,
-        inputs=[hidden_recorder, audio_state],
+        inputs=[recorder, audio_state],
         outputs=audio_state,
-        time_limit=150,
+        time_limit=30,
         stream_every=0.5
     )
 
-    # при остановке записи
-    hidden_recorder.stop_recording(
+    # при остановке записи (пользователем или как истечёт время)
+    recorder.stop_recording(
         fn=lambda: call.final_audio_path,
-        outputs=hidden_player
+        outputs=player2
+    ).then(
+        fn=lambda: [gr.update(visible=False), gr.update(visible=False), gr.update(visible=False)],
+        outputs=[recorder_text, recorder, recorder_final_text]
+    ).then(
+        fn=update_visibility,
+        outputs=[player_end_text, player2]
     ).then(
         fn=call.process_full_audio,
         inputs=audio_state,
-        outputs=[routing_result]
+        outputs=[routing_result, audio_state]
+    )
+
+    # когда воспроизведение второй записи останавливается (появляется финальное поле)
+    player2.stop(
+        fn=update_invisibility,
+        outputs=[player_end_text, player2]
     ).then(
-        fn=lambda: {"full_buffer": [], "partial_buffer": []},  # очистка
-        outputs=audio_state
+        fn=lambda: gr.update(visible=True),
+        outputs=routing_result
     )
 
 
